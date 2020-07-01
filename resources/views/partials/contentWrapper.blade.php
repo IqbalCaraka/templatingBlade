712a29d38c1cfb91ec1a0aa79d8b14d6df4852cc
IqbalCaraka <iqbalcaraka@gmail.com>
2020-07-01 18:32:32 +0700

    edit yield

--- resources/views/partials/contentWrapper.blade.php
+++ resources/views/partials/contentWrapper.blade.php
@@ -4,10 +4,10 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
+          <div class="col-sm-10">
             @yield('content')
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-2">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Blank Page</li>
